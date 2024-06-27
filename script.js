7c18dc2494b9b277e0a73fe63f5b66591d44b275
Pinguin130 <104287658+Pinguin130@users.noreply.github.com>
2024-06-27 02:33:01 -0300

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 window.onload = function() {
     let progress = 0;
     const loadingText = document.getElementById('loadingText');
-    const loadingBar = document.querySelector('.loading-bar::before');
+    const loadingProgress = document.querySelector('.loading-progress');
     const loadingScreen = document.querySelector('.loading-screen');
     const message = document.querySelector('.message');
 
     const interval = setInterval(() => {
         progress += 1;
         loadingText.textContent = progress + '%';
-        loadingBar.style.transform = `translateY(${100 - progress}%)`;
+        loadingProgress.style.transform = `translateY(${100 - progress}%)`;
 
         if (progress >= 100) {
             clearInterval(interval);
             loadingScreen.classList.add('hidden');
             message.classList.remove('hidden');
         }
-    }, 50); // Adjust the speed by changing the interval duration
+    }, 50); // Ajusta la velocidad cambiando la duración del intervalo
 };
